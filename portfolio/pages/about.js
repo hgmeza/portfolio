df8c91ac6201af7e31808a4bcb4cc6813fb64c5c
Hugo Meza <hgmeza114@gmail.com>
2019-12-05 23:10:52 -0500

    Add simple header component

--- portfolio/pages/about.js
+++ portfolio/pages/about.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
 import Header from '../components/shared/Header';
 
-class Index extends Component {
+class About extends Component {
   render() {
     return (
       <div>
+        <h1>About page</h1>
         <Header/>
+        
       </div>
     )
   }
 }
 
-export default Index;
+export default About;
---
+++ portfolio/pages/cv.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Cv extends Component {
+  render() {
+    return (
+      <h1>Cv page</h1>
+    )
+  }
+}
+
+export default Cv;
